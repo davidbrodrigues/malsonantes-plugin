4e3c3ac79ed1390d2e32ae784d33762e3cc34048
davidbrodrigues <dbragarodrigues@danielcastelao.org>
2022-11-30 19:26:58 +0100

    plugin blacklist final con arrays

--- html/wp-content/plugins/malsonantes/malsonantes.php
+++ html/wp-content/plugins/malsonantes/malsonantes.php
@@ -8,14 +8,13 @@ Version: 1.0
 /*
  * Remplaza las palabras malas por las buenas
  */
-add_filter( 'the_content', 'filtro_palabras_malsonantes', 1 );
+add_filter( 'the_content', 'filtro_palabras_malsonantes' );
 
 function filtro_palabras_malsonantes( $content ) {
 
     $malsonantes = array('gilipollas', 'subnormal', 'idiota', 'tonto');
-    $reemplazo = array('palabra_blacklist');
+    $reemplazo = array('palabra_blacklist', 'palabra_blacklist', 'palabra_blacklist', 'palabra_blacklist', );
 
-    str_replace($malsonantes, $reemplazo, $content);
+    return str_replace($malsonantes, $reemplazo, $content);
 
-    return $content;
 }
