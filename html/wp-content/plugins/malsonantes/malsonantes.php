0b0ecfb3ea33b3f5008e9557b7b3c3164bd27261
davidbrodrigues <dbragarodrigues@danielcastelao.org>
2022-11-30 21:54:03 +0100

    plugin blacklist final con base de datos

--- html/wp-content/plugins/malsonantes/malsonantes.php
+++ html/wp-content/plugins/malsonantes/malsonantes.php
@@ -1,21 +1,77 @@
 <?php
 /*
-Plugin Name: Plugin malsonantes david
+Plugin Name: Plugin malsonantes david + Base de datos
 Plugin URI: http://www.davidbr.org/
-Description: blacklist de palabras malsonantes
-Version: 1.0
+Description: blacklist de palabras malsonantes Base de datos
+Version: 1.1
 */
 /*
  * Remplaza las palabras malas por las buenas
  */
-add_filter( 'the_content', 'filtro_palabras_malsonantes' );
-/*creacion de la funcion*/
-function filtro_palabras_malsonantes( $content ) {
-    /*creacion de arrays de palabras malsonantes y el reemplazo*/
+function malsonantes( $text ) {
+    // Objeto WP DB
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    //prefijo de tabla
+    $table_name = $wpdb->prefix . 'malsonantes';
+    //con al funcion SELECT recogemos todos los datos de la tabla
+    $datostabla = $wpdb->get_results("SELECT * FROM " . $table_name, ARRAY_A);
+    //arrays que contendran las malsonantes y el reemplazo
+    $malsonantes = array();
+    $reemplazo = array();
+    //recorremos los datos recogidos
+    foreach($datostabla as $fila)
+    {
+        // añadimos las palabras
+        array_push($malsonantes, $fila['malsonantes']);
+        array_push($reemplazo, $fila['reemplazo']);
+    }
+
+    return str_replace( $malsonantes, $reemplazo, $text );
+}
+    //añadimos un filtro al contenido con la funcion malsonantes
+add_filter( 'the_content', 'malsonantes' );
+
+function myplugin_update_db_check() {
+
+    // Objeto WP DB
+    global $wpdb;
+    // recojemos el
+    $charset_collate = $wpdb->get_charset_collate();
+    // le añado el prefijo a la tabla
+    $table_name = $wpdb->prefix . 'malsonantes';
+    //sentencia sql
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        malsonantes varchar (30),
+        reemplazo varchar (20),
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+    //añadimos los requerimientos
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    //añadimos la funcion dbDelta que modifica la base de datos segun lo que especifiquemos
+    dbDelta( $sql );
+    // creamos los arrays de las palabras para añadir a la sql
     $malsonantes = array('gilipollas', 'subnormal', 'idiota', 'tonto');
     $reemplazo = array('palabra_blacklist', 'palabra_blacklist', 'palabra_blacklist', 'palabra_blacklist', );
+    //igualamos el id a 0
+    $i = 0;
+    //añadimos mas requerimientos
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    // insertamos en la sql
+    foreach($malsonantes as $malsonantes){
+        $result = $wpdb->insert(
+            $table_name,
+            array(
+                'id' => $i,
+                'malsonantes' => $malsonantes,
+                'reemplazo' => $reemplazo[$i]
+            )
+        );
+        //va sumando 1 a la id
+        $i++;
+    }
 
-    /*la funcion que reemplaza las palabras y nos devuelve el cambio*/
-    return str_replace($malsonantes, $reemplazo, $content);
-
+    error_log("Plugin DAM insert: " . $result);
 }
+add_action( 'plugins_downloaded', 'myplugin_update_db_check' );
